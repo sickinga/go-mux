2d0485f016781a95da41cd50fa2cf3c7f9d57910
Marc Kurz <marc.kurz@fh-hagenberg.at>
2020-04-28 12:49:25 +0200

    added comments

--- main.go
+++ main.go
@@ -11,5 +11,10 @@ func main() {
 		os.Getenv("APP_DB_PASSWORD"),
 		os.Getenv("APP_DB_NAME"))
 
+	//export APP_DB_USERNAME=postgres
+	//export APP_DB_PASSWORD=
+	//export APP_DB_NAME=postgres
+
+	//List env variables (bash): printenv
 	a.Run(":8010")
 }
