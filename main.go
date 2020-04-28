04bbc0aae2841da04857dbb0e137ea2217302a58
Marc Kurz <marc.kurz@fh-hagenberg.at>
2020-04-28 15:14:38 +0200

    modified main.go for env variable credentials

--- main.go
+++ main.go
@@ -2,15 +2,14 @@
 
 package main
 
+import "os"
+
 func main() {
 	a := App{}
 	a.Initialize(
-		//os.Getenv("APP_DB_USERNAME"),
-		//os.Getenv("APP_DB_PASSWORD"),
-		//os.Getenv("APP_DB_NAME"))
-		"postgres",
-		"",
-		"postgres")
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
 
 	//export APP_DB_USERNAME=postgres
 	//export APP_DB_PASSWORD=<whatever password you use>
