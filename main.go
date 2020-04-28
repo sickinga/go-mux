dac4c3bcbfcfe2291ca198258f8778b10cee02ef
Marc Kurz <marc.kurz@fh-hagenberg.at>
2020-04-28 14:55:37 +0200

    added credentials for DB for Travis

--- main.go
+++ main.go
@@ -2,14 +2,15 @@
 
 package main
 
-import "os"
-
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		//os.Getenv("APP_DB_USERNAME"),
+		//os.Getenv("APP_DB_PASSWORD"),
+		//os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"",
+		"postgres")
 
 	//export APP_DB_USERNAME=postgres
 	//export APP_DB_PASSWORD=<whatever password you use>
