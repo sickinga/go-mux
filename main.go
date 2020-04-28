5baedb1ad4eb8452a62fcb163d6ec43abd1169fa
Marc Kurz <marc.kurz@fh-hagenberg.at>
2020-04-28 13:13:54 +0200

    added hint in comments for password

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 		os.Getenv("APP_DB_NAME"))
 
 	//export APP_DB_USERNAME=postgres
-	//export APP_DB_PASSWORD=
+	//export APP_DB_PASSWORD=<whatever password you use>
 	//export APP_DB_NAME=postgres
 
 	//List env variables (bash): printenv
